432e4d8eb9a75879d013536f9681db5b749bb6eb
Ajay <jaykumar002kori@gmail.com>
2024-12-31 18:36:21 +0530

    commit

--- Antenna/views.py
+++ Antenna/views.py
@@ -1,8 +1,24 @@
-from django.shortcuts import render
+import os  
+import sys
+# Add the parent directory to the sys.path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from django.shortcuts import render 
 from django.http import HttpResponse
+from utils import CustomLogger
+
+
+## Create a logger object
+custom_logger = CustomLogger.CustomLogger()
+logger = custom_logger.get_logger()
+
 
 # Create your views here.
-
 def home(request):
-    return render(request, 'index.html')
+    try:
+        #logger.info("Home page accessed")
+        return render(request, 'index.html')
+    except Exception as e:
+        #logger.error(f"Error in home page: {e}")
+        return HttpResponse("Error in home page")
 
